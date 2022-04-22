f4107f7d81168b7236fc15a54f149fcadf79ab75
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2022-04-22 16:26:20 +0200

    Create VirtualKeyboard.py

---
+++ ComputerVision/VirtualKeyboard/VirtualKeyboard.py
@@ -0,0 +1,7 @@
+# https://faun.pub/virtual-keyboard-using-computer-vision-32b61981c5b1
+# Documentation: google.github.io/mediapipe/solutions/hands.html
+# key press simulation: https://pyautogui.readthedocs.io/en/latest/
+
+import cv2
+import mediapipe as mp
+import pyautogui as pg
