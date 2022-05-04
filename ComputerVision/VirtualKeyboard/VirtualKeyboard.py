ec292f653f768d4a125a4813fecb39dedb52da13
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2022-05-04 11:13:01 +0200

    Enforcing pep8 with flake8

--- ComputerVision/VirtualKeyboard/VirtualKeyboard.py
+++ ComputerVision/VirtualKeyboard/VirtualKeyboard.py
@@ -1,8 +1,9 @@
 '''
 Summary:
-        Virtual keyboard making use of computer vision, OpenCV, and handtracking via mediapipe.
+        Virtual keyboard making use of computer vision, OpenCV, and 
+        handtracking via mediapipe.
 Sources:
-        Inspiration: https://faun.pub/virtual-keyboard-using-computer-vision-32b61981c5b1
+        Inspiration: https://faun.pub/virtual-keyboard-using-computer-vision-32b61981c5b1 # noqa
         Documentation handtracker: google.github.io/mediapipe/solutions/hands.html
         Key press simulation: https://pyautogui.readthedocs.io/en/latest/
         Source caps lock status: https://localcoder.org/python-3-detect-caps-lock-status
@@ -15,90 +16,95 @@ Author:
         JM
 '''
 
-if __name__ == "__main__": # To indicate that this is a run file and not a library.
+if __name__ == "__main__":
+    import cv2
+    import mediapipe as mp
+    import pyautogui as pg
+    import numpy as np
+    from function_file import *
 
-        # Import packages
-        import cv2
-        import mediapipe as mp
-        import pyautogui as pg
-        import numpy as np
-        from function_file import *
+    # Setting up camera
+    camerainput = 0  # default 0
+    cap = cv2.VideoCapture(camerainput)
+    w = 1280
+    h = 720
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
 
-        # Setting up camera
-        camerainput = 0 # default 0
-        cap = cv2.VideoCapture(camerainput)
-        w = 1280
-        h = 720
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+    # Declare hands object from mediapipe
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=False,
+                           max_num_hands=2,
+                           min_detection_confidence=0.5,
+                           min_tracking_confidence=0.5)
 
-        # Declare hands object from mediapipe
-        mp_hands = mp.solutions.hands
-        hands = mp_hands.Hands(static_image_mode=False,
-                        max_num_hands=2,
-                        min_detection_confidence=0.5,
-                        min_tracking_confidence=0.5)
+    # Landmark numbers
+    index_finger_tip = 8
 
-        # Landmark numbers
-        index_finger_tip = 8
+    # Virtual keyboard
+    keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "capslock"],
+            ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "enter"],
+            ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "space"]]
 
-        # Virtual keyboard
-        keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P","capslock"],
-                ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";","enter"],
-                ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/","space"]]
+    # Key sizes as a function of the camera window
+    start_h = int(h*0.75)
+    key_h = int(h*0.25/3)
+    key_w = int(w/len(keys[0]))
 
-        # Key sizes as a function of the camera window
-        start_h = int(h*0.75)
-        key_h = int(h*0.25/3)
-        key_w = int(w/len(keys[0]))
+    # Letter position relative to box
+    letter_w = int(np.ceil(key_w*0.25))
+    letter_h = int(np.ceil(key_h*0.8))
 
-        # Letter position relative to box
-        letter_w = int(np.ceil(key_w*0.25))
-        letter_h = int(np.ceil(key_h*0.8))
+    # Set up for pressAfterXSeconds
+    new_run = True
+    start_time = 0
+    letter_start = 'Q'
 
-        # Set up for pressAfterXSeconds
-        new_run = True
-        start_time = 0
-        letter_start = 'Q'
+    button_pos_list = get_button_pos_list(key_w, key_h, start_h, keys)
+    pg.press("capslock")  # start with lower letters
 
-        button_pos_list = get_button_pos_list(key_w, key_h, start_h, keys)
-        pg.press("capslock") # start with lower letters
+    while True:
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        while True:
-                success, img = cap.read()
-                img = cv2.flip(img,1)
-                imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        draw_virtual_keyboard(
+            img, key_w, key_h, button_pos_list, letter_w, letter_h)
 
-                draw_virtual_keyboard(img, key_w, key_h, button_pos_list, letter_w, letter_h)
+        # Show caps lock status
+        CAPSLOCK = CAPSLOCK_STATE()
+        if ((CAPSLOCK) & 0xffff) != 0:
+            cv2.putText(img, "Lower", (int(w*0.8), 70),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+        elif ((CAPSLOCK) & 0xffff) == 0:
+            cv2.putText(img, "Upper", (int(w*0.8), 70),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
 
-                # Show caps lock status
-                CAPSLOCK = CAPSLOCK_STATE()
-                if ((CAPSLOCK) & 0xffff) != 0:
-                        cv2.putText(img, "Lower", (int(w*0.8),70), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3) 
-                elif ((CAPSLOCK) & 0xffff) == 0:
-                        cv2.putText(img, "Upper", (int(w*0.8),70), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3) 
+        # Hand detection + hand landmark markingG
+        results = hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+            for hand_no, hand_lms in enumerate(results.multi_hand_landmarks):
+                for id, lm in enumerate(hand_lms.landmark):
+                    if id == index_finger_tip:  # Annotate part of hand
+                        cx, cy = int(lm.x * w), int(lm.y*h)
+                        cv2.circle(img, (cx, cy), 10, (0, 0, 255), -1)
 
-                # Hand detection + hand landmark markingG
-                results = hands.process(imgRGB)
-                if results.multi_hand_landmarks:
-                        for hand_no, hand_lms in enumerate(results.multi_hand_landmarks):
-                                for id, lm in enumerate(hand_lms.landmark):
-                                        if id == index_finger_tip: # Annotate part of hand
-                                                cx, cy = int(lm.x *w), int(lm.y*h)
-                                                cv2.circle(img, (cx,cy), 10, (0,0,255), -1)
+                        # Button pressing (selects after maintaining the buttonpress for 2 seconds)
+                        for pos, letter in button_pos_list:
+                            # Index finger within a key box
+                            condition = (
+                                pos[0] < cx < pos[0] + key_w) & (pos[1] < cy < pos[1] + key_h)
+                            if condition:
+                                new_run, start_time, letter_start = press_after_x_seconds(
+                                    img, letter, 2, new_run, start_time, letter_start)  # Hold for 2 seconds to write
 
-                                                # Button pressing (selects after maintaining the buttonpress for 2 seconds)
-                                                for pos, letter in button_pos_list:
-                                                        condition = (pos[0] < cx < pos[0]+ key_w) & (pos[1] < cy < pos[1]+ key_h) # Index finger within a key box
-                                                        if condition:
-                                                                new_run, start_time, letter_start = press_after_x_seconds(img, letter, 2, new_run, start_time, letter_start) # Hold for 2 seconds to write
-                                                                        
-                # Display camera image
-                cv2.imshow("Image", img)
-                if cv2.waitKey(1) & 0xFF == 27: #escape is set as the quitting button
-                        print('break')
-                        if ((CAPSLOCK) & 0xffff) != 0:
-                                pg.press("capslock") # Turn caps lock of when shutting down.
+        # Display camera image
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == 27:  # escape is set as the quitting button
+            print('break')
+            if ((CAPSLOCK) & 0xffff) != 0:
+                pg.press("capslock")  # Turn caps lock of
+                # when shutting down.
 
-                        cv2.destroyAllWindows()
-                        break
+            cv2.destroyAllWindows()
+            break
--- ComputerVision/VirtualKeyboard/function_file.py
+++ ComputerVision/VirtualKeyboard/function_file.py
@@ -3,148 +3,154 @@ import time
 import ctypes
 import pyautogui as pg
 
-# Save button startpoint
+
 def get_button_pos_list(key_w, key_h, start_h, keys):
-  '''Gets a list of tuples containing the startpoint of the key and the letter.
-  
-  Parameters
-  ----------
-  keys: list
-          The letters to display ordered.
-  key_w: int
-          Key box width.
-  key_h: int
-          Key box heigth.
-  start_h: int
-          Start height of the whole keyboard.
+    '''Gets a list of tuples containing the startpoint of the key and the letter.
 
-  Returns
-  ----------
-  button_pos_list: list
-        List of tuples containing the startpoint of the key and the letter.
-  '''
+    Parameters
+    ----------
+    keys: list
+            The letters to display ordered.
+    key_w: int
+            Key box width.
+    key_h: int
+            Key box heigth.
+    start_h: int
+            Start height of the whole keyboard.
 
-  button_pos_list = []
-  j = -1
-  for ar in keys:
-          j +=1
-          i = 0
-          for key in ar:
-                  startpoint = (key_w*i,     start_h + key_h*j)
-                  tup = (startpoint, key)
-                  button_pos_list.append(tup)
-                  i +=1
-  return button_pos_list
+    Returns
+    ----------
+    button_pos_list: list
+          List of tuples containing the startpoint of the key and the letter.
+    '''
+
+    button_pos_list = []
+    j = -1
+    for ar in keys:
+        j += 1
+        i = 0
+        for key in ar:
+            startpoint = (key_w*i,     start_h + key_h*j)
+            tup = (startpoint, key)
+            button_pos_list.append(tup)
+            i += 1
+    return button_pos_list
+
 
 def draw_key(img, key, pos, key_w, key_h, letter_w, letter_h):
-  '''
-  Function to draw a key button on the camera screen.
+    '''
+    Function to draw a key button on the camera screen.
 
-  Parameters
-  ----------
-  img: numpy.ndarray
-          Image to draw on.
-  key: str
-          Letter to draw.
-  pos: tuple
-          Start position of the keybutton
-  key_w: int
-          Key box width.
-  key_h
-          Key box height.
-  letter_w
-          Letter width.
-  letter_h
-          Letter height.
-  '''
-  # Draw boxes for keyboard
-  startpoint = (pos[0],        pos[1])
-  endpoint   = (pos[0]+ key_w, pos[1]+ key_h)
-  colour = (255, 0, 0)
-  thick = -1
-  colour_border = (255,255,255)
-  thick_border = 1
-  cv2.rectangle(img, startpoint, endpoint, colour, thick)
-  cv2.rectangle(img, startpoint, endpoint, colour_border, thick_border) # Border
+    Parameters
+    ----------
+    img: numpy.ndarray
+            Image to draw on.
+    key: str
+            Letter to draw.
+    pos: tuple
+            Start position of the keybutton
+    key_w: int
+            Key box width.
+    key_h
+            Key box height.
+    letter_w
+            Letter width.
+    letter_h
+            Letter height.
+    '''
+    # Draw boxes for keyboard
+    startpoint = (pos[0],        pos[1])
+    endpoint = (pos[0] + key_w, pos[1] + key_h)
+    colour = (255, 0, 0)
+    thick = -1
+    colour_border = (255, 255, 255)
+    thick_border = 1
+    cv2.rectangle(img, startpoint, endpoint, colour, thick)
+    cv2.rectangle(img, startpoint, endpoint,
+                  colour_border, thick_border)  # Border
 
-  # Draw letter 
-  org = (letter_w+pos[0], letter_h+pos[1])
-  font_letter = cv2.FONT_HERSHEY_DUPLEX
-  font_size = 1
-  font_color = (255,255,255)
-  font_thick = 2
-  cv2.putText(img,key,org,font_letter,font_size,font_color,font_thick)
+    # Draw letter
+    org = (letter_w+pos[0], letter_h+pos[1])
+    font_letter = cv2.FONT_HERSHEY_DUPLEX
+    font_size = 1
+    font_color = (255, 255, 255)
+    font_thick = 2
+    cv2.putText(img, key, org, font_letter, font_size, font_color, font_thick)
 
-# Draw keyboard
+
 def draw_virtual_keyboard(img, key_w, key_h, button_pos_list, letter_w, letter_h):
-  '''Draws a virtual keyboard on the bottom of your image
-  
-  Parameters
-  ----------
-  img: numpy.ndarray
-          Image to draw on.
-  key_w: int
-          Key box width.
-  key_h: int
-          Key box heigth.
-  button_pos_list: list
-          List of tuples containing button position and the letter itself.
-  '''
-  for pos,key in button_pos_list:
-          draw_key(img, key, pos, key_w, key_h, letter_w, letter_h)
+    '''Draws a virtual keyboard on the bottom of your image
 
-# Button press after x seconds
+    Parameters
+    ----------
+    img: numpy.ndarray
+            Image to draw on.
+    key_w: int
+            Key box width.
+    key_h: int
+            Key box heigth.
+    button_pos_list: list
+            List of tuples containing button position and the letter itself.
+    '''
+    for pos, key in button_pos_list:
+        draw_key(img, key, pos, key_w, key_h, letter_w, letter_h)
+
+
 def press_after_x_seconds(img, letter, wait_sec, new_run, start_time, letter_start):
-  '''
-  Write the letter after maintaining position for X seconds.
-  Need to put the output back into the function and define the start variables.
+    '''
+    Write the letter after maintaining position for X seconds.
+    Need to put the output back into the function and define the start variables.
 
-  Parameters
-  ----------
-  img: numpy.array
-    Image to draw on.
-  letter: str
-    Letter currently selected by the user.
-  wait_sec: int
-    Number of seconds to maintain the hold.
-  new_run: bool
-    Determines whether a new key is being selected.
-  start_time: float
-    Start time of the key being held.
-  letter_start: str
-    The letter being held.
+    Parameters
+    ----------
+    img: numpy.array
+      Image to draw on.
+    letter: str
+      Letter currently selected by the user.
+    wait_sec: int
+      Number of seconds to maintain the hold.
+    new_run: bool
+      Determines whether a new key is being selected.
+    start_time: float
+      Start time of the key being held.
+    letter_start: str
+      The letter being held.
 
-  Returns
-  ----------
-  new_run: bool
-    Determines whether a new key is being selected.
-  start_time: float
-    Start time of the key being held.
-  letter_start: str
-    The letter being held.
-  '''
+    Returns
+    ----------
+    new_run: bool
+      Determines whether a new key is being selected.
+    start_time: float
+      Start time of the key being held.
+    letter_start: str
+      The letter being held.
+    '''
 
-  if (new_run): # Start counting if condition satisfied
-          letter_start = letter
-          start_time = time.time()
-          new_run = False
-  elif ((time.time()-start_time >= wait_sec) & (letter_start == letter) & ~new_run): # if more than 2 seconds, write
-          print(letter)
-          pg.press(letter) # Actual write, open a text document to see output
-          new_run = True
-  elif ((time.time()-start_time < wait_sec) & (letter_start == letter) & ~new_run): # if less than 2 seconds, wait
-          cv2.putText(img, f'{time.time()-start_time:.2f}', (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,255,255), 3) # Show hold timer
-  elif ((letter_start != letter) & ~new_run): # if index finger on different key, restart timer
-          new_run = True
+    if (new_run):  # Start counting if condition satisfied
+        letter_start = letter
+        start_time = time.time()
+        new_run = False
+    # if more than 2 seconds, write
+    elif ((time.time()-start_time >= wait_sec) & (letter_start == letter) & ~new_run):
+        print(letter)
+        pg.press(letter)  # Actual write, open a text document to see output
+        new_run = True
+    # if less than 2 seconds, wait
+    elif ((time.time()-start_time < wait_sec) & (letter_start == letter) & ~new_run):
+        cv2.putText(img, f'{time.time()-start_time:.2f}', (10, 70),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)  # Show hold timer
+    elif ((letter_start != letter) & ~new_run):  # if index finger on different key, restart timer
+        new_run = True
 
-  return new_run, start_time, letter_start
+    return new_run, start_time, letter_start
+
 
 def CAPSLOCK_STATE():
-  '''
-  Returns the state of capslock.
-  Capslock on -> value not equal to zero.
-  Capslock off -> value equal to zero.
-  '''
-  hllDll = ctypes.WinDLL ("User32.dll")
-  VK_CAPITAL = 0x14
-  return hllDll.GetKeyState(VK_CAPITAL)
+    '''
+    Returns the state of capslock.
+    Capslock on -> value not equal to zero.
+    Capslock off -> value equal to zero.
+    '''
+    hllDll = ctypes.WinDLL("User32.dll")
+    VK_CAPITAL = 0x14
+    return hllDll.GetKeyState(VK_CAPITAL)
