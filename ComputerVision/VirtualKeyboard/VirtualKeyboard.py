3f5f69a8c9ebee7bf73cb849a52e22c78b4fae5e
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2022-05-04 09:58:27 +0200

    add constructor

--- ComputerVision/VirtualKeyboard/VirtualKeyboard.py
+++ ComputerVision/VirtualKeyboard/VirtualKeyboard.py
@@ -14,88 +14,91 @@ Usage:
 Author: 
         JM
 '''
-# Import packages
-import cv2
-import mediapipe as mp
-import pyautogui as pg
-import numpy as np
-from function_file import *
 
-# Setting up camera
-camerainput = 0 # default 0
-cap = cv2.VideoCapture(camerainput)
-w = 1280
-h = 720
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+if __name__ == "__main__": # To indicate that this is a run file and not a library.
 
-# Declare hands object from mediapipe
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False,
-                      max_num_hands=2,
-                      min_detection_confidence=0.5,
-                      min_tracking_confidence=0.5)
+        # Import packages
+        import cv2
+        import mediapipe as mp
+        import pyautogui as pg
+        import numpy as np
+        from function_file import *
 
-# Landmark numbers
-index_finger_tip = 8
+        # Setting up camera
+        camerainput = 0 # default 0
+        cap = cv2.VideoCapture(camerainput)
+        w = 1280
+        h = 720
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
 
-# Virtual keyboard
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P","capslock"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";","enter"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/","space"]]
+        # Declare hands object from mediapipe
+        mp_hands = mp.solutions.hands
+        hands = mp_hands.Hands(static_image_mode=False,
+                        max_num_hands=2,
+                        min_detection_confidence=0.5,
+                        min_tracking_confidence=0.5)
 
-# Key sizes as a function of the camera window
-start_h = int(h*0.75)
-key_h = int(h*0.25/3)
-key_w = int(w/len(keys[0]))
+        # Landmark numbers
+        index_finger_tip = 8
 
-# Letter position relative to box
-letter_w = int(np.ceil(key_w*0.25))
-letter_h = int(np.ceil(key_h*0.8))
+        # Virtual keyboard
+        keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P","capslock"],
+                ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";","enter"],
+                ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/","space"]]
 
-# Set up for pressAfterXSeconds
-new_run = True
-start_time = 0
-letter_start = 'Q'
+        # Key sizes as a function of the camera window
+        start_h = int(h*0.75)
+        key_h = int(h*0.25/3)
+        key_w = int(w/len(keys[0]))
 
-button_pos_list = get_button_pos_list(key_w, key_h, start_h, keys)
-pg.press("capslock") # start with lower letters
+        # Letter position relative to box
+        letter_w = int(np.ceil(key_w*0.25))
+        letter_h = int(np.ceil(key_h*0.8))
 
-while True:
-        success, img = cap.read()
-        img = cv2.flip(img,1)
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # Set up for pressAfterXSeconds
+        new_run = True
+        start_time = 0
+        letter_start = 'Q'
 
-        draw_virtual_keyboard(img, key_w, key_h, button_pos_list, letter_w, letter_h)
+        button_pos_list = get_button_pos_list(key_w, key_h, start_h, keys)
+        pg.press("capslock") # start with lower letters
 
-        # Show caps lock status
-        CAPSLOCK = CAPSLOCK_STATE()
-        if ((CAPSLOCK) & 0xffff) != 0:
-                cv2.putText(img, "Lower", (int(w*0.8),70), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3) 
-        elif ((CAPSLOCK) & 0xffff) == 0:
-                cv2.putText(img, "Upper", (int(w*0.8),70), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3) 
+        while True:
+                success, img = cap.read()
+                img = cv2.flip(img,1)
+                imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        # Hand detection + hand landmark markingG
-        results = hands.process(imgRGB)
-        if results.multi_hand_landmarks:
-                for hand_no, hand_lms in enumerate(results.multi_hand_landmarks):
-                        for id, lm in enumerate(hand_lms.landmark):
-                                if id == index_finger_tip: # Annotate part of hand
-                                        cx, cy = int(lm.x *w), int(lm.y*h)
-                                        cv2.circle(img, (cx,cy), 10, (0,0,255), -1)
+                draw_virtual_keyboard(img, key_w, key_h, button_pos_list, letter_w, letter_h)
 
-                                        # Button pressing (selects after maintaining the buttonpress for 2 seconds)
-                                        for pos, letter in button_pos_list:
-                                                condition = (pos[0] < cx < pos[0]+ key_w) & (pos[1] < cy < pos[1]+ key_h) # Index finger within a key box
-                                                if condition:
-                                                        new_run, start_time, letter_start = press_after_x_seconds(img, letter, 2, new_run, start_time, letter_start) # Hold for 2 seconds to write
-                                                                
-        # Display camera image
-        cv2.imshow("Image", img)
-        if cv2.waitKey(1) & 0xFF == 27: #escape is set as the quitting button
-                print('break')
+                # Show caps lock status
+                CAPSLOCK = CAPSLOCK_STATE()
                 if ((CAPSLOCK) & 0xffff) != 0:
-                        pg.press("capslock") # Turn caps lock of when shutting down.
+                        cv2.putText(img, "Lower", (int(w*0.8),70), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3) 
+                elif ((CAPSLOCK) & 0xffff) == 0:
+                        cv2.putText(img, "Upper", (int(w*0.8),70), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 3) 
 
-                cv2.destroyAllWindows()
-                break
+                # Hand detection + hand landmark markingG
+                results = hands.process(imgRGB)
+                if results.multi_hand_landmarks:
+                        for hand_no, hand_lms in enumerate(results.multi_hand_landmarks):
+                                for id, lm in enumerate(hand_lms.landmark):
+                                        if id == index_finger_tip: # Annotate part of hand
+                                                cx, cy = int(lm.x *w), int(lm.y*h)
+                                                cv2.circle(img, (cx,cy), 10, (0,0,255), -1)
+
+                                                # Button pressing (selects after maintaining the buttonpress for 2 seconds)
+                                                for pos, letter in button_pos_list:
+                                                        condition = (pos[0] < cx < pos[0]+ key_w) & (pos[1] < cy < pos[1]+ key_h) # Index finger within a key box
+                                                        if condition:
+                                                                new_run, start_time, letter_start = press_after_x_seconds(img, letter, 2, new_run, start_time, letter_start) # Hold for 2 seconds to write
+                                                                        
+                # Display camera image
+                cv2.imshow("Image", img)
+                if cv2.waitKey(1) & 0xFF == 27: #escape is set as the quitting button
+                        print('break')
+                        if ((CAPSLOCK) & 0xffff) != 0:
+                                pg.press("capslock") # Turn caps lock of when shutting down.
+
+                        cv2.destroyAllWindows()
+                        break
